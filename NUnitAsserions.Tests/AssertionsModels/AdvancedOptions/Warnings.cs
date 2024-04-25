b55916634a1202c0e41a5448dd6dba80f46ab5f1
Piotr Lisewski <p.lisewski@hotmail.com>
2024-04-25 19:43:25 +0200

    add using warnings

---
+++ NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/Warnings.cs
@@ -0,0 +1,14 @@
+﻿namespace NUnitAsserions.Tests.AssertionsModels.AdvancedOptions
+{
+    public class Warnings
+    {
+        [Test]
+        public void UsingWarnings()
+        {
+            var isProcessed = false;
+
+            Warn.Unless(isProcessed, Is.EqualTo(true).After(10).Seconds.PollEvery(10).Seconds);
+            Console.WriteLine("warned, not failed");
+        }
+    }
+}
