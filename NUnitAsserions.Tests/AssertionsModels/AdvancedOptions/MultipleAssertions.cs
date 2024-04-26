70fb191cc2b0838808e12109699f70013cf5d4b3
Piotr Lisewski <p.lisewski@hotmail.com>
2024-04-26 19:04:10 +0200

    add multiple (soft) assertions

---
+++ NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/MultipleAssertions.cs
@@ -0,0 +1,16 @@
+﻿namespace NUnitAsserions.Tests.AssertionsModels.AdvancedOptions
+{
+    public class MultipleAssertions
+    {
+        [Test]
+        public void TestMultipleAssertions()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(1, Is.EqualTo(1));
+                Assert.That("foo", Is.Not.EqualTo("boo"));
+                Assert.That(true, Is.True);
+            });
+        }
+    }
+}
