54e7c1047b69792f78146ee32e5e59bfee105fed
Piotr Lisewski <p.lisewski@hotmail.com>
2024-04-26 19:00:52 +0200

    implement remained utility assertions

--- NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/UtilityAssertions.cs
+++ NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/UtilityAssertions.cs
@@ -11,19 +11,19 @@
         [Test]
         public void Assert_Fail()
         {
-
+            Assert.That(Assert.Fail, Throws.TypeOf<AssertionException>());
         }
 
         [Test]
         public void Assert_Ignore()
         {
-
+            Assert.That(Assert.Ignore, Throws.TypeOf<IgnoreException>());
         }
 
         [Test]
         public void Assert_Inconclusive()
         {
-
+            Assert.That(Assert.Inconclusive, Throws.TypeOf<InconclusiveException>());
         }
     }
 }
