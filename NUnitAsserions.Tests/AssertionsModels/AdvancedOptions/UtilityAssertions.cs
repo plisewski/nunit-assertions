87229e15ed4f0d91a04d7e73e71d61ed67f8dc0a
Piotr Lisewski <p.lisewski@hotmail.com>
2024-04-26 18:50:56 +0200

    add utility assertions

---
+++ NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/UtilityAssertions.cs
@@ -0,0 +1,29 @@
+﻿namespace NUnitAsserions.Tests.AssertionsModels.AdvancedOptions
+{
+    public class UtilityAssertions
+    {
+        [Test]
+        public void Assert_Pass()
+        {
+            Assert.That(Assert.Pass, Throws.TypeOf<SuccessException>());
+        }
+
+        [Test]
+        public void Assert_Fail()
+        {
+
+        }
+
+        [Test]
+        public void Assert_Ignore()
+        {
+
+        }
+
+        [Test]
+        public void Assert_Inconclusive()
+        {
+
+        }
+    }
+}
