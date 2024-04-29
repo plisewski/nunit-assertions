08a8e9b9053343a9e804483f9e7a2f09fda43c00
Piotr Lisewski <p.lisewski@hotmail.com>
2024-04-29 12:48:56 +0200

    add assumptions

---
+++ NUnitAsserions.Tests/AssertionsModels/AdvancedOptions/Assumptions.cs
@@ -0,0 +1,20 @@
+﻿namespace NUnitAsserions.Tests.AssertionsModels.AdvancedOptions;
+
+public class Assumptions
+{
+    private string _expected = "nunit";
+    private string _actual = "nunit";
+
+    [Test]
+    public void AssumptionsTest()
+    {
+        var customerSettingsEnabled = false;
+        Assume.That(customerSettingsEnabled, Is.True);
+
+        // if state in assumption is not met NUnit aborts a test (but does not fail it)
+        // it's particualry useful e.g. in case the SUT is highly configurable
+
+        Assert.That(_actual, Is.EqualTo(_expected));
+
+    }
+}
